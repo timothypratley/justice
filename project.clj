f365d7874ea9461eb16afc2eddc3b64dddfb2776
Timothy Pratley <timothypratley@gmail.com>
2019-04-02 00:59:00 -0700

    Set initial version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject justice "0.1.0-SNAPSHOT"
+(defproject justice "0.0.1-alpha"
   :description "Concise rule definition and query syntax for DataScript"
   :url "https://github.com/timothypratley/justice"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
